1fb7fe80cd5a9a23b19bb3eea67a828390be31f9
Tassio2002git config --global user.email tassio.santos2002@gmail.com <tassio.santos2002@gmail.com>
2023-05-31 07:34:31 -0300

    :construction: add routes

--- src/app.spec.tsx
+++ src/app.spec.tsx
@@ -1,7 +1,7 @@
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-describe("Test App Component", () => {
+describe("Test render App component", () => {
   it("should render login component", () => {
     const { getByText } = render(<App />);
 
@@ -19,7 +19,7 @@ describe("Test App Component", () => {
   it("should render email input", () => {
     const { getByPlaceholderText } = render(<App />);
 
-    expect(getByPlaceholderText("Username")).toBeInTheDocument();
+    expect(getByPlaceholderText("Email")).toBeInTheDocument();
   });
 
   it("should render password input", () => {
@@ -32,5 +32,55 @@ describe("Test App Component", () => {
     const { getByText } = render(<App />);
 
     expect(getByText("Enter")).toBeInTheDocument();
+  });
+});
+// teste para email inválido
+describe("Test input validation", () => {
+  const onSubmit = jest.fn();
+  it("would fail to send email field", () => {
+    onSubmit.mockImplementation((event) => {
+      event.preventDefault();
+    });
+    const { getByText, getByPlaceholderText } = render(<App />);
+    const emailInput = getByPlaceholderText("Email");
+    const enterButton = getByText("Enter");
+
+    fireEvent.input(emailInput, "test");
+    fireEvent.click(enterButton);
+
+    // eslint-disable-next-line no-restricted-globals
+    expect(location.pathname).toEqual("/")
+  });
+// teste para senha inválida
+  it("would fail to send password field", () => {
+    onSubmit.mockImplementation((event) => {
+      event.preventDefault();
+    });
+    const { getByText, getByPlaceholderText } = render(<App />);
+    const passwordInput = getByPlaceholderText("Password");
+    const enterButton = getByText("Enter");
+
+    fireEvent.input(passwordInput, "test123");
+    fireEvent.click(enterButton);
+
+    // eslint-disable-next-line no-restricted-globals
+    expect(location.pathname).toEqual("/")
+  });
+// teste para login valido
+  it("should enter the calculator page", () => {
+    onSubmit.mockImplementation((event) => {
+      event.preventDefault();
+    });
+    const { getByText, getByPlaceholderText } = render(<App />);
+    const emailInput = getByPlaceholderText("Email");
+    const passwordInput = getByPlaceholderText("Password");
+    const enterButton = getByText("Enter");
+
+    fireEvent.input(emailInput, "tassio.santos@simbioseventures.com");
+    fireEvent.input(passwordInput, "SenhaForte8!");
+    fireEvent.click(enterButton);
+
+    // eslint-disable-next-line no-restricted-globals
+    expect(location.pathname).toEqual("/calculator")
   });
 });
--- src/components/LoginBox.tsx
+++ src/components/LoginBox.tsx
@@ -1,27 +1,41 @@
+import { useNavigate } from "react-router-dom";
+
 export const LoginBox = () => {
+  const navigate = useNavigate();
+
+  function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    navigate("/calculator");
+  }
+
   return (
-    <div className="w-1/3 py-6 flex flex-col items-center bg-black">
+    <form
+      onSubmit={handleSubmit}
+      className="w-1/3 py-6 flex flex-col items-center bg-black"
+    >
       <h1 className="pb-2 text-4xl text-white">Login</h1>
       <span className="pb-4 text-base text-gray-500">
         Please enter your login and password!
       </span>
       <input
         type="email"
+        required
+        placeholder="Email"
         className="w-1/2 p-3 border-2 border-blue-400 rounded-full mb-5 text-white bg-black placeholder:text-center placeholder:text-gray-500 focus-visible:outline-none"
-        placeholder="Username"
       />
 
       <input
         type="password"
-        className="w-1/2 p-3 border-2 border-blue-400 rounded-full mb-5 text-white bg-black placeholder:text-center placeholder:text-gray-500 focus-visible:outline-none"
+        required
         placeholder="Password"
+        className="w-1/2 p-3 border-2 border-blue-400 rounded-full mb-5 text-white bg-black placeholder:text-center placeholder:text-gray-500 focus-visible:outline-none"
       />
 
       <input
-        type="button"
+        type="submit"
         value="Enter"
         className="w-1/4 p-3 border-2 border-green-400 rounded-xl text-white bg-black cursor-pointer hover:bg-green-400"
       />
-    </div>
+    </form>
   );
 };
